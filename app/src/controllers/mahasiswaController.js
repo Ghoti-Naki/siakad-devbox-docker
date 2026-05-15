e2eb2eaab1877974aa415b8dfa37f37e531fd9cf
Ghoti-Naki <dhikaferdiansyah19@gmail.com>
2026-05-15 20:43:51 +0700

    fix controller error

--- app/src/controllers/mahasiswaController.js
+++ app/src/controllers/mahasiswaController.js
@@ -1,33 +1,3 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const controller = require('../controllers/mahasiswaController');
-
-//filtr file
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'application/pdf' || file.mimetype.startsWith('image/')) {
-    cb(null, true);
-  } else {
-    cb(new Error('Format file tidak didukung! Gunakan PDF atau Gambar.'), false);
-  }
-};
-
-const upload = multer({ 
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 }, 
-  fileFilter: fileFilter
-});
-
-//route mhs
-router.get('/', controller.index);
-router.get('/mahasiswa/create', controller.createForm);
-router.post('/mahasiswa', upload.single('dokumen'), controller.store);
-router.get('/mahasiswa/:id/edit', controller.editForm);
-router.post('/mahasiswa/:id/update', controller.update);
-router.post('/mahasiswa/:id/delete', controller.destroy);
-
-module.exports = router;
-
 const pool = require('../config/db');
 const minioClient = require('../config/minio');
 const BUCKET_NAME = process.env.MINIO_BUCKET || 'student-documents';
